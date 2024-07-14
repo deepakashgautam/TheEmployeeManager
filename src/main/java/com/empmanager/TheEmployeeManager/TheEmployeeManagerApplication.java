42b010292514ac94189019fc7d67ee857771f121
DEEPAKASH GAUTAM <deepakashgautam@gmail.com>
2024-07-14 13:04:53 +0530

    Initial commit

---
+++ src/main/java/com/empmanager/TheEmployeeManager/TheEmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.empmanager.TheEmployeeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheEmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheEmployeeManagerApplication.class, args);
+	}
+
+}
